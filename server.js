3af8e15f6140921fa534d2dc8fe0f890c90b2700
Heric Macedo Oliveira Silva <heric_commercial@hotmail.com>
2020-06-11 22:36:51 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+// .env
+require('dotenv').config()
+
+
+// server config
+const server = require('express')();
+server.set('url', process.env.URL || 'http://localhost');
+server.set('port', process.env.PORT || 3000);
+
+
+// bodyParser config
+const bodyParser = require('body-parser');
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
+
+// Loading Routes
+const routes = require('./api/routes/routes');
+const testRoutes = require('./api/routes/testRoutes');
+// ...
+
+
+// Using Routes
+server.use(routes);
+server.use('/test', testRoutes);
+// ...
+
+
+// Server Start
+const url = server.get('url');
+const port = server.get('port');
+server.listen(port, () => {
+    console.log(`Running: "${url}:${port}/"`);
+});
+
